bdc9e74c66d90ee1858453f49dc94f930ba97d8e
Sukant Jha <61352224+itssukant@users.noreply.github.com>
2020-08-23 13:08:39 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  title: 'My Site',
-  tagline: 'The tagline of my site',
+  title: 'Sukant Jha',
+  tagline: 'Still learning the things',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
+  organizationName: 'itssukant', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'My Site',
+      title: 'Sukant Jha',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
